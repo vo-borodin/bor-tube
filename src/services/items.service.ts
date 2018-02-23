79c2758eb11a66f21641b0c5f53811a172d78787
vo-borodin <brdnvsvd89@mail.ru>
2018-02-23 20:28:26 +0700

    Initial commit

---
+++ src/services/items.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+import { HttpInterceptorService, RESTService } from '@covalent/http';
+import { MOCK_API } from '../config/api.config';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ItemsService extends RESTService<any> {
+
+  constructor(private _http: HttpInterceptorService) {
+    super(_http, {
+      baseUrl: MOCK_API,
+      path: '/items',
+    });
+  }
+
+  staticQuery(): any {
+    return this._http.get('data/items.json')
+    .map((res: Response) => {
+      return res.json();
+    });
+  }
+
+  staticGet(id: string): any {
+    return this._http.get('data/items.json')
+    .map((res: Response) => {
+      let item: any;
+      res.json().forEach((s: any) => {
+        if (s.item_id === id) {
+          item = s;
+        }
+      });
+      return item;
+    });
+  }
+}
