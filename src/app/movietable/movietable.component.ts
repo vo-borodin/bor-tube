a0ed0758f4adc1fa66bfff7471d4b608c249d3a1
DATAEAST\vborodin <vborodin@dataeast.com>
2018-02-28 14:04:36 +0700

    the search field is added to the main page and the loading of table is attached to change of its value

--- src/app/movietable/movietable.component.ts
+++ src/app/movietable/movietable.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 import { MovieService } from '../../services/movie.service';
 import { Movie } from '../../models/movie.model';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import { merge } from 'rxjs/observable/merge';
 import { switchMap } from 'rxjs/operators/switchMap';
 import { startWith } from 'rxjs/operators/startWith';
@@ -19,21 +21,28 @@ export class MovietableComponent implements OnInit {
   itemsCount$ = 0;
   itemsPerPage$ = 10;
   movies: Movie[];
+  searchStr = "";
+  const subject = new Subject();
   
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private movieService: MovieService) { }
   
+  onModelChange(value) {
+    this.searchStr = value;
+    this.subject.next(value);
+  }
+  
   ngOnInit() {
-    merge(this.paginator.page).pipe(
+    merge(this.paginator.page, this.subject).pipe(
       startWith({}),
       switchMap(() => {
-          return this.movieService.getMovies(this.paginator.pageIndex + 1);
+        return this.movieService.getMovies(this.paginator.pageIndex + 1);
       }),
       map(data => {
-          this.itemsPerPage$ = data.results.length;
-          this.itemsCount$ = data.total_results;
-          return data.results;
+        this.itemsPerPage$ = data.results.length;
+        this.itemsCount$ = data.total_results;
+        return data.results;
       })).subscribe(movies => this.movies = movies);
   }
 }
